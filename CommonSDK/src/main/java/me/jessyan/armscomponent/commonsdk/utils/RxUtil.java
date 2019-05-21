13942a4beabb291bf78132963b573bc0dd334bb8
jason <itlanguege@163.com>
2019-05-21 11:57:27 +0800

    Update RxUtil

--- CommonSDK/src/main/java/me/jessyan/armscomponent/commonsdk/utils/RxUtil.java
+++ CommonSDK/src/main/java/me/jessyan/armscomponent/commonsdk/utils/RxUtil.java
@@ -18,6 +18,8 @@ package me.jessyan.armscomponent.commonsdk.utils;
 import com.jess.arms.mvp.IView;
 import com.jess.arms.utils.RxLifecycleUtils;
 
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
 import io.reactivex.ObservableTransformer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -95,5 +97,22 @@ public class RxUtil {
                         }).compose(RxLifecycleUtils.bindToLifecycle(view));
             }
         };
+    }
+
+    /**
+     * 倒计时
+     *
+     * @param maxTime
+     * @return
+     */
+    public static Observable<Integer> countDown(int maxTime) {
+        return Observable.interval(0, 1, TimeUnit.SECONDS)
+                .take(maxTime + 1)//只发射开始的N项数据或者一定时间内的数据
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .map(aLong -> {
+                    // 做数据的变化，因为interval执行along会从0开始递增，我们需要的是剩余值
+                    return maxTime - aLong.intValue();
+                })
+                .observeOn(AndroidSchedulers.mainThread());
     }
 }
